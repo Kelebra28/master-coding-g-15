9a6df94aab1d52971dcb3c66ebd37db1c765c379
Kelebra <ricardokelebra@gmail.com>
2022-03-07 20:46:21 -0600

    intro js

---
+++ 02_Intro_Js/01_Intro_JS_Basico/main.js
@@ -0,0 +1,70 @@
+//  sintaxis de variables
+//  ->
+// var nombre = valor 
+
+// Tipo numerico
+var num1 = 10
+
+// Tipo texto
+var saludo = 'Hola mundo' // -> son comillas simples o dobles
+
+// Tipo Booleano
+var verdadero = false // -> true
+
+
+var num2 = 20
+
+num1 + num2
+
+// Salida de datos
+//  console.log(datos que quiero ver)
+// alert(datos que quiero ver)
+// Mala practica usar Alert :(
+// alert('Hola mundo')
+// console.log('Hola mundo')
+
+var suma = num1 + num2
+// console.log(suma)
+
+// Entradas
+// prompt -> Una ventaja emergente que recibe informacion
+// var pregunta = prompt('Ingres tu nombre')
+
+// console.log(pregunta)
+
+// Operadores
+
+ // suma  resta  multiplicacion  division
+//  +       -      *                /  
+
+// cambio de valor en variables
+var humor = 'Feli'
+
+// console.log(humor)
+
+var humor = 'Triste'
+// console.log(humor)
+
+humor = 100
+// console.log(humor)
+
+
+var nombre = 'Ricardo'
+var edad = 23
+
+console.log('Ricardo tiene 23')
+
+console.log(nombre + ' ' + 'tiene' + ' ' + edad)
+console.log(nombre + ' tiene ' + edad)
+
+
+// Ejercicios 
+
+// 1.- Preguntar la edad, el nombre, el apellido, el telefono y la direccion del usuario
+// -> Nombre: ....
+// -> Apellido: ....
+// -> Telefono: ....
+// -> Direccion: ....
+
+// 2.- Van a preguntar su año de nacimiento y en la consola pondran
+// que edad tienen/van a cumplir este año 
