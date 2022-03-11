920b06c3efbd7756397fe386ab3097040b456996
Kelebra <ricardokelebra@gmail.com>
2022-03-10 20:50:36 -0600

    ciclo for

---
+++ 02_Intro_Js/03_Ciclo_For/main.js
@@ -0,0 +1,37 @@
+
+
+// Sintaxis del ciclo For 
+
+// for (  inicio;  limite; secuencia ) {
+//     bloque de codigo
+// }
+
+for(var i = 0; i <= 10; i = i + 2){
+    // console.log(i)
+}
+
+for(var y = 10; y >= 0; y--){
+    // console.log(y)
+}
+
+
+for(var x = 100; x <= 200; x = x + 10) {
+    if(x === 150){
+        console.log('Es mi condicion: ' + x )
+    }else {
+        console.log(x)
+    }
+}
+
+
+// Recorrer los numeros del 1 al 100 y decir cual es inpar y cual es par
+// % -> modulo no da el residuo de la division
+
+// Fizz bozz
+// Recorrer los numeros del 1 al 100
+// si el numoero es divisible entre 5 -> 5: Fizz
+// Si el numero es divisible entre 3 -> 3: Bozz
+// Si el numero es divisible entre 3 y 5 ->  15: FizzBozz
+
+// 15 Fizz
+// 15 Bozz
